be030383067f0457ce29bb0b8e705458987ed771
Hunter <hlupro@gmail.com>
2020-04-29 02:46:28 -0400

    updated

--- projectDriver.cpp
+++ projectDriver.cpp
@@ -4,7 +4,7 @@
 int main() {
   // std::cout << "===========GRAPH_COLORING AND SUDOKU=================" << std::endl;
   //
-  int sudokuGrid = 4;
+  int sudokuGrid = 9;
   //
   // std::cout << "4x4 or 9x9?" << std::endl;
   // std::cin>> sudokuGrid;
